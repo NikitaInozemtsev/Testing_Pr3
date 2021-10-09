be356e8343c82ea34a5e2d8d40909a779093f891
Nikita <inozemcevniki@gmail.com>
2021-10-09 10:50:07 +0300

    Логика работы метода getPattern

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +12,17 @@ public class Main {
     public static File[] getFilesFromDirectory() {
         File folder = new File("src/patterns");
         return folder.listFiles();
+    }
+
+    public static String getPattern(File[] files, int index) throws IOException {
+        FileReader reader = new FileReader(files[index]);
+
+        Scanner scanner = new Scanner(reader);
+        StringBuilder result = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            result.append(scanner.nextLine() + "\n");
+        }
+        reader.close();
+        return result.toString();
     }
 }
