b443eccc22e61bf0af79820f9650cefb6344d22b
Nikita <inozemcevniki@gmail.com>
2021-10-09 10:47:28 +0300

    Тест для  метода getPattern

--- untitled/src/TestMain.java
+++ untitled/src/TestMain.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 
@@ -10,6 +11,14 @@ public class TestMain {
     public void getFilesFromDirectory() {
         assertEquals(21, Main.getFilesFromDirectory().length);
         assertEquals(new File("src/patterns/Adapter"), Main.getFilesFromDirectory()[0]);
+    }
+
+    @Test
+    public void getPatternTest() throws IOException {
+        File[] files = {new File("src/patterns/Adapter")};
+        assertEquals(485, Main.getPattern(files, 0).length());
+        assertEquals("class PBank {\n", Main.getPattern(files, 0).substring(0, 14));
+
     }
 
 }
