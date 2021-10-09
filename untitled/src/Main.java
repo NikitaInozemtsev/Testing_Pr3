47378bf0e674722c1110c09e08a5b6ddb2e39bf3
Nikita <inozemcevniki@gmail.com>
2021-10-09 10:46:25 +0300

    Логика работы метода getFilesFromDirectory

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,13 @@
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("some text");
 
+    }
+
+    public static File[] getFilesFromDirectory() {
+        File folder = new File("src/patterns");
+        return folder.listFiles();
     }
 }
